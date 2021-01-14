d64090aff63ba7bc96d1c07014dac2ee21213967
taajuddin <taajraza12@gmail.com>
2021-01-14 10:00:29 +0530

    working on menu

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow,Menu} = require('electron')
 
 let mainWindow
 
@@ -11,8 +11,50 @@ function createMainWindow(){
     //mainWindow.loadURL(`file://${__dirname}/app/index.html`)
     mainWindow.loadFile('./app/index.html')
 }
-app.on('ready',createMainWindow)
+app.on('ready',()=>{
+  createMainWindow()
+  const mainMenu=Menu.buildFromTemplate(menu)
+  Menu.setApplicationMenu(mainMenu)
+  mainWindow.on('closed',()=> mainWindow = null)
+})
 
+const menu= [
+  {
+    label: 'File',
+    submenu:[
+      {
+        label:'Quit',
+        click:()=>app.quit()
+      }
+    ]
+  },
+  {
+    label: 'View',
+    submenu: [
+      { role: 'reload' },
+      { role: 'forceReload' },
+      { role: 'toggleDevTools' },
+      { type: 'separator' },
+      { role: 'resetZoom' },
+      { role: 'zoomIn' },
+      { role: 'zoomOut' },
+      { type: 'separator' },
+      { role: 'togglefullscreen' }
+    ]
+  },
+  {
+    role: 'help',
+    submenu: [
+      {
+        label: 'Learn More',
+        click: async () => {
+          const { shell } = require('electron')
+          await shell.openExternal('https://electronjs.org')
+        }
+      }
+    ]
+  }
+]
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
